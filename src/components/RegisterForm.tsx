de99d9f69e627d3b0459b250d7d65059e94d4f3c
Bazska2013 <balazstakacs0330@gmail.com>
2026-06-12 20:08:31 +0200

    Registration form's basic styling is done.

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -2,31 +2,43 @@ const RegisterForm = () => {
     return (
         <div className="registerForm">
             <h1>Regisztráció</h1>
-            <p>Add meg az e-mail címed és jelszavad a regisztrációhoz!</p>
+            <p className="subTitle">Add meg az e-mail címed és jelszavad a regisztrációhoz!</p>
             <div id="textInputs">
                 <div id="nameRow">
                     <div className="inputField">
                         <label htmlFor="lastName">Vezetéknév<span>*</span></label>
-                        <input name="lastName" type="text" placeholder="Teszt"/>
+                        <div className="inputFieldBackground">
+                            <input name="lastName" type="text" placeholder="Teszt"/>
+                        </div>
                     </div>
                     <div className="inputField">
                         <label htmlFor="firstName">Keresztnév<span>*</span></label>
-                        <input name="firstName" type="text" placeholder="Elek"/>
+                        <div className="inputFieldBackground">
+                            <input name="firstName" type="text" placeholder="Elek"/>
+                        </div>
                     </div>
                 </div>
                 <div className="inputField">
                     <label htmlFor="email">Email cím<span>*</span></label>
-                    <input name="email" type="email" placeholder="example@mail.com"/>
+                    <div className="inputFieldBackground">
+                        <input name="email" type="email" placeholder="example@mail.com"/>
+                    </div>
                 </div>
                 <div className="inputField">
                     <label htmlFor="password">Jelszó<span>*</span></label>
-                    <input name="password" type="email" placeholder="********"/>
+                    <div className="inputFieldBackground">
+                        <input name="password" type="password" placeholder="********"/>
+                    </div>
                 </div>
             </div>
-            <input name="accept" type="text" className="checkbox"/>
-            <label htmlFor="accept">Fiók létrehozásával elfogadja a <a>Felhasználási feltételeket</a>, és az <a>Adatvédelmi irányelveinket</a></label>
-            <button className="submitButton">Regisztráció</button>
-            <p>Már van fiókja? <a>Bejelentkezés</a></p>
+            <div className="checkField">
+                <input name="accept" type="checkbox" className="checkbox"/>
+                <label htmlFor="accept">Fiók létrehozásával elfogadja a <a href="">Felhasználási feltételeket</a>, és az <a href="">Adatvédelmi irányelveinket</a></label>
+            </div>
+            <div id="submitButton" className="inputFieldBackground">
+                <button className="bigButton">Regisztráció</button>
+            </div>
+            <p id="hasAccount">Már van fiókja? <a href="">Bejelentkezés</a></p>
             <div className="orDivider">
                 <div className="line"></div>
                 <div className="text">Vagy</div>
